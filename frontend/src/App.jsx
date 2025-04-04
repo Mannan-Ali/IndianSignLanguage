16122c176bac6cd4aa5005aacc4086847bf07a6e
mannan <alimannan.am244@gmail.com>
2025-04-04 22:12:04 +0530

    Add setup for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Sign Language Detection</h1>
+      <img src="http://localhost:5000/video_feed" alt="Live Video" />
+    </>
+  )
+}
+
+export default App
